52ec34d856d097fb55db3f4b0c133421bd829fc9
aneebamoezz <anibakaleem@gmail.com>
2024-03-07 04:30:07 +0500

    commit initial files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import StarRating from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Star Rating</h1>
+      <StarRating />
+    </div>
+  );
+}
+
+export default App;
